a51ca00e5e42af1c84e812df3f3c7589dcfb083d
codyjacques <cjacques@sagecloud.com>
2014-08-19 20:42:10 -0400

    challenge 2

--- Set1/Challenge2/xOrEqualBuffers.py
+++ Set1/Challenge2/xOrEqualBuffers.py
@@ -3,15 +3,19 @@ import argparse
 
 # parses the arguments and calls the function that does the work
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-b1', '--first-buffer', required=True)
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-b1', '--first-buffer', required=True)
 	parser.add_argument('-b2', '--second-buffer', required=True)
-    args = vars(parser.parse_args())
+	args = vars(parser.parse_args())
 
-    xOrValues(args['first_buffer'], args['second_buffer'])
+	xOrValues(args['first_buffer'], args['second_buffer'])
 
 # This function will take two buffers and return the xor of them
 def xOrValues(buffer1, buffer2):
-	pass
+	intVal = int(buffer1, 16)
+	secondVal = int(buffer2, 16)
+	result = hex( int(intVal) ^ int(secondVal) )[2:][:-1]
+	print result
+	return result
 
 main()
